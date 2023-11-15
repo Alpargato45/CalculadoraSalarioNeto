9c2a964827140e3710cd7ee61bfb3df2f1b5c030
Alpargato45 <jorgedelcid2004@gmail.com>
2023-11-15 13:46:52 +0100

    añadido funcionalidad comboBox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,11 +30,11 @@ namespace CalculadoraSalarioNeto
             if(checkBoxMovilidad.IsChecked == true)
             {
                 //Al pulsar el checkBox había que quitarle un 1%
-                porcentaje = porcentaje - 1;
+                porcentaje += - 1;
             }else if(checkBoxMovilidad.IsChecked != false)
             {
                 //Si quita la selección de la casilla vuelve al estado original
-                porcentaje = porcentaje + 1;
+                porcentaje += 1;
             }
         }
 
@@ -42,5 +42,27 @@ namespace CalculadoraSalarioNeto
         //Cojo el número de sueldos para luego dividir el sueldo dependiendo de las veces que va a cobrar al año
         private void radioButton12_Checked(object sender, RoutedEventArgs e) {numSueldos = 12;}
         private void radioButton14_Checked(object sender, RoutedEventArgs e) {numSueldos = 14;}
+
+
+        //Método del ComboBox
+        private void comboboxFamiliar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Hago un switch para cada caso del propio comboBox
+            string situacionSeleccionada = comboboxFamiliar.SelectedItem as string;
+            switch (situacionSeleccionada) {
+                case "Soltero":
+                    numSueldos += 2;
+                    break;
+                case "Viudo":
+                    numSueldos += -2;
+                    break;
+                case "Divorciado":
+                    numSueldos += -1;
+                    break;
+                default:
+                    numSueldos += 0;
+                    break;
+            }
+        }
     }
 }
