7cc04b418850813025fc29bd1a135f25152afad7
Jorge del Cid <jorgedelcid2004@gmail.com>
2023-11-16 14:02:53 +0100

    disminución de código

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CalculadoraSalarioNeto
 {
@@ -22,34 +11,19 @@ namespace CalculadoraSalarioNeto
         private double porcentaje = 0;
         private int numSueldos = 0;
         private double sueldo = 0;
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() { InitializeComponent(); }
 
         //Método al pulsar el CheckBox de Movilidad
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            if(checkBoxMovilidad.IsChecked == true)
-            {
-                //Al pulsar el checkBox había que quitarle un 1%
-                porcentaje += - 1;
-            }else if(checkBoxMovilidad.IsChecked != false)
-            {
-                //Si quita la selección de la casilla vuelve al estado original
-                porcentaje += 1;
-            }
-        }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) { porcentaje += - 1; }
 
 
         //Cojo el número de sueldos para luego dividir el sueldo dependiendo de las veces que va a cobrar al año
-        private void radioButton12_Checked(object sender, RoutedEventArgs e) {numSueldos = 12;}
-        private void radioButton14_Checked(object sender, RoutedEventArgs e) {numSueldos = 14;}
+        private void radioButton12_Checked(object sender, RoutedEventArgs e) {numSueldos = 12; }
+        private void radioButton14_Checked(object sender, RoutedEventArgs e) {numSueldos = 14; } 
 
 
         //Método del ComboBox
-        private void comboboxFamiliar_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        private void comboboxFamiliar_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             //Hago un switch para cada caso del propio comboBox
             string situacionSeleccionada = comboboxFamiliar.SelectedItem as string;
             switch (situacionSeleccionada) {
@@ -70,10 +44,7 @@ namespace CalculadoraSalarioNeto
 
 
         //Métodos ToggleButton
-        private void toggleButtonDiscapacidad_Checked(object sender, RoutedEventArgs e)
-        {
-            numSueldos += -5;
-        }
+        private void toggleButtonDiscapacidad_Checked(object sender, RoutedEventArgs e) { numSueldos += -5; }
 
 
         //Método TextBox
